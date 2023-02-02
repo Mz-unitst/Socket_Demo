6524ba080f7544bf853e5f2c014ab26dd69350f3
mz <1764293088@qq.com>
2023-02-02 12:02:59 +0800

    实现了传输，转为大写功能 未实现缓存清除

--- main.c
+++ main.c
@@ -1,47 +1,73 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <time.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-
-void checkResult (int ResCode, char* methods );
-
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "netdb.h"
 int main() {
-    printf("Hello, Linuxxx!\n");
-    checkResult(-1, "test");
-    int listenfd, connfd; //句柄
+//    unlink("server_socket");
+    int listenfd, connfd; //文件描述符
     socklen_t len;
-    struct sockaddr_in servaddr, cliaddr;
-    char buff[10086];
+    struct sockaddr_in servaddr, cliaddr;// ip port 文件
+    char buff[105];
+    int portt=9003;
+    int res,nbytes;
     time_t ticks;
+    struct hostent *hp= gethostbyname("mzunitst.top");
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);//SOCK_STREAM为字节流套接字
     bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(9001);
+    servaddr.sin_family = AF_INET;//IVP4
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);// 0.0.0.0 接受所有IP的请求 network long 32b IP
+    servaddr.sin_port = htons(portt);//服务器端口 short 16b端口
+    printf("start...\t");
 
-    checkResult(bind(listenfd, &servaddr, sizeof(servaddr)), "bind");
-    checkResult(listen(listenfd, 10), "listen");//10个队列?
-    printf("bind && listen success!\n");
+    res=bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+    if(res<0){
+        perror("bind");
+        return 0;
+    }
+    res=listen(listenfd, 5);
+    if(res<0){
+        perror("listen");
+        return 0;
+    }
+    printf("finish\n");
 
-    for (;;) {
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+    while(1) {
+
         len = sizeof(cliaddr);
-        checkResult(connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len),"accept");
-        inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff));
-        printf("connection from %s, port %d\n", buff, ntohs(cliaddr.sin_port));
-        ticks = time(NULL);
-        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        printf("buff:%s\n",buff);
-        write(connfd, buff, strlen(buff));
+        connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len);
+        if(connfd<0){
+            perror("accept");
+            return 0;
+        }
+        inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff));//将二进制IP转换为点分十进制的字符串IP
+        printf("connection from %s, port %d\n", buff, ntohs(cliaddr.sin_port));// ntos :net to short ,将 net的port 转为短整型
+
+        nbytes=0;
+        char s[101];
+        if (nbytes<3){
+            nbytes += read(connfd, s, sizeof (s)); //字符串 strlen sizeof read的参数
+            printf("strlen: %d,s: %s\n", strlen(s),s);
+            printf("readd %d bytes content from client: %s\n", nbytes, s);
+        }
+
+        for(int i=0;i<nbytes;i++){
+            if(s[i]>='a' && s[i]<='z')
+            s[i]=s[i]-32;
+        }
+        write(connfd,s, nbytes);
         close(connfd);
+        printf("closed\n\n");
     }
+#pragma clang diagnostic pop
 }
 
-void checkResult (int ResCode, char* methods ){
-    if(ResCode<0){
-        printf("%s ERROR\n",methods);
-    }
-}
+
 
